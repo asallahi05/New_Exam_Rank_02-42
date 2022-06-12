a89fff049fbe7ff77d816f342589ec220e6cfb1a
Ajjig <majjig@pm.me>
2022-06-12 21:06:31 +0100

    fizzbuzz is easy to remember rn

--- level01/fizzbuzz/fizzbuzz.c
+++ level01/fizzbuzz/fizzbuzz.c
@@ -1,31 +1,23 @@
 #include <unistd.h>
-#include <stdio.h>
 
-void	ft_write_number(int number)
+void	ft_putnbr(int nb)
 {
-	char	str[10] = "0123456789";
-
-	if (number > 9)
-		ft_write_number(number / 10);
-	write (1, &str[number % 10], 1);
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	write (1, &"0123456789"[nb % 10], 1);
 }
 
-int		main()
+int	main()
 {
-	int		i;
-
-	i = 1;
-	while (i <= 100)
+	int i = -1;
+	while (++i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			write (1, "fizzbuzz", 8);
-		else if (i % 3 == 0)
-			write (1, "fizz", 4);
-		else if (i % 5 == 0)
-			write (1, "buzz", 4);
-		else
-			ft_write_number(i);
-		i++;
+		if (!(i % 3))
+			write(1, "fizz", 4);
+		if (!(i % 5))
+			write(1, "buzz", 4);
+		if (i % 3 && i % 5)
+			ft_putnbr(i);
 		write (1, "\n", 1);
 	}
 }
